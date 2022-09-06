2a454644981935a2ddf1ec998ebc71f79b307d1f
Jake Lee <51874481+dragonsnapx@users.noreply.github.com>
2022-09-06 21:35:11 +0900

    Barrel file & npm publishing fix

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Patcher from "./Patcher";
+import PreparedPatchFile from "./PreparedPatchFile";
+import {crc32} from "./utils/crc";
+import { BPSPattern } from "./patterns/BPSPattern";
+import { IPSPattern } from "./patterns/IPSPattern";
+import { UPSPattern } from "./patterns/UPSPattern";
+
+export {Patcher, PreparedPatchFile, crc32, BPSPattern, IPSPattern, UPSPattern };
